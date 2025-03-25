7c4910aef08d7acbe83faf3101bbd881656f32fc
EduardoKazan <eduardo_kazan@hotmail.com>
2025-03-25 20:42:52 -0300

    first commit

--- PlatformIO/Projects/OTA/.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- PlatformIO/Projects/OTA/.pio/build/upesy_wroom/lib52d/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/upesy_wroom/lib52d/WebServer/detail/mimetable.cpp.o: \
- C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
