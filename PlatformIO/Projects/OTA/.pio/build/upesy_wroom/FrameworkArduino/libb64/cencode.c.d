76eb28b60b79a0f43c08f141c454a810781d3fc0
EduardoKazan <eduardo_kazan@hotmail.com>
2025-03-25 20:38:57 -0300

    new files included

---
+++ PlatformIO/Projects/OTA/.pio/build/upesy_wroom/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/upesy_wroom/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
---
+++ PlatformIO/Projects/OTA/.pio/build/upesy_wroom/lib52d/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/upesy_wroom/lib52d/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/eduar/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
